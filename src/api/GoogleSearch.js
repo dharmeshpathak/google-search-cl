511d3ef90d8a672d3beee80a81a38e67cb634fd5
dharmesh <dharmeshpathak1999@gmail.com>
2022-03-27 21:53:21 -0600

    api udpated

--- src/api/GoogleSearch.js
+++ src/api/GoogleSearch.js
@@ -15,11 +15,7 @@ export const SearchData = async (term) => {
   const { data } = await axios.get(
     'https://www.googleapis.com/customsearch/v1',
     {
-    //   params: {
-    //     key: "AIzaSyB2cKn4YRwB_zDyvSC0WyU2aZSOyaKtOlo",
-    //     cx: "2ca1b8b9860939878",
-    //     q: term,
-    //   },
+    
       params: {
         key: process.env.React_App_Google_Search_Api_Key,
         cx: process.env.React_App_Search_Engine_Id,
